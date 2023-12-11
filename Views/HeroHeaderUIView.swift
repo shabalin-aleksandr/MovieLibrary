2971dd29d1abf80a2ca1efa20ca0ce0421090b61
shabalin-aleksandr <sasha19822010@gmail.com>
2023-12-11 22:20:34 +0100

    feat: home page (wip)

---
+++ Views/HeroHeaderUIView.swift
@@ -0,0 +1,20 @@
+//
+//  HeroHeaderUIView.swift
+//  MovieLibrary
+//
+//  Created by Aleksandr Shabalin on 11.12.2023.
+//
+
+import UIKit
+
+class HeroHeaderUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
