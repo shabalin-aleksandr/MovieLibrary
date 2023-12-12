d705452e947345c253e475dbb454a27ac73cf2c4
shabalin-aleksandr <sasha19822010@gmail.com>
2023-12-12 01:53:56 +0100

    feat: search functionality

--- Managers/APICaller.swift
+++ Managers/APICaller.swift
@@ -91,4 +91,44 @@ class APICaller {
         }
         response.resume()
     }
+    
+    func getDiscoverMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let url = URL(string: "\(Configuration.baseURL)/3/discover/movie?api_key=\(Configuration.API_KEY)&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate") else {return }
+        let response = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+
+            } catch {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        response.resume()
+    }
+    
+    func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {return}
+        guard let url = URL(string: "\(Configuration.baseURL)/3/search/movie?api_key=\(Configuration.API_KEY)&query=\(query)") else {
+            return
+        }
+        
+        let response = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+        
+            do {
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
+                completion(.success(results.results))
+
+            } catch {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        response.resume()
+    }
 }
